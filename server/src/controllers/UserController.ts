83b3392306f061c9bbf506d00921daaccab3970c
Allen Elguira <AllenElguira16@gmail.com>
2020-02-13 12:42:13 +0800

    yeah

--- server/src/controllers/UserController.ts
+++
@@ -1,7 +0,0 @@
-import { Get } from "@tsed/common";
-
-export default class UserController {
-  // Registers user
-  @Get()
-  registerUser = () => {};
-}
--- web/src/types.d.ts
+++
@@ -1,6 +0,0 @@
-interface RegisterInput {
-  email: string;
-  password: string;
-  reEnterPassword: string;
-  profilePicture: string;
-}
